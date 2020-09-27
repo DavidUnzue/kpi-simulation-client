5dd6bfb982703f795be1da818243647385209850
David Unzue <davidunzue@gmail.com>
2020-09-27 15:43:51 +0200

    Updates

---
+++ precache-manifest.21193a7b3e900b6595769867a5e1caac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb6ac156c97842af20a9d112c45a83e8",
+    "url": "/kpi-simulation-client/index.html"
+  },
+  {
+    "revision": "fa2068824d15cef73150",
+    "url": "/kpi-simulation-client/static/css/main.4e59d86d.chunk.css"
+  },
+  {
+    "revision": "174cfba477973480953e",
+    "url": "/kpi-simulation-client/static/js/2.ea7631ec.chunk.js"
+  },
+  {
+    "revision": "0fb0e672f1308cb5b7677cdb93fc42ab",
+    "url": "/kpi-simulation-client/static/js/2.ea7631ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa2068824d15cef73150",
+    "url": "/kpi-simulation-client/static/js/main.d0b5e8dd.chunk.js"
+  },
+  {
+    "revision": "bf097ba5931f36060229",
+    "url": "/kpi-simulation-client/static/js/runtime-main.70fba5f6.js"
+  },
+  {
+    "revision": "c93ff43d2f2f7f4c178d08f7b8072269",
+    "url": "/kpi-simulation-client/static/media/location.c93ff43d.svg"
+  }
+]);
